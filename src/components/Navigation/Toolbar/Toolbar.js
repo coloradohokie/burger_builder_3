6c069c6da4ef8b733670eeb18dbbe2e2ec1d5952
Michael Newman <coloradohokie@gmail.com>
2020-12-11 13:41:11 -0700

    add navigation items placeholders to toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
 import classes from './Toolbar.module.scss'
+
 
 const toolbar = (props) => (
 
     <header className={classes.Toolbar}>
         <div>MENU</div>
         <Logo />
-        <nav>
-            ...
-        </nav>
+        <NavigationItems />
     </header>
 )
 
