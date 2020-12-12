50ceddbfb3867c1ae3e6208f667383a4f6bfefb1
Michael Newman <coloradohokie@gmail.com>
2020-12-12 12:23:31 -0700

    move layout; add component should update to modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-    const ingedientSummary = Object.entries(props.ingredients)
-        .map((entry) => {
-            const [name, amount] = entry
-            return (
-                <li key={name}>
-                    <span style={{textTransform: 'capitalize'}}>{name}</span>: {amount}
-                </li>
-            )
-        })
+class OrderSummary extends Component {
+    componentDidUpdate() {
+        console.log('[OrderSummary] DidUpdate')
+    }
 
-    return(
-        <>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients</p>
-            <ul>
-                {ingedientSummary}
-            </ul>
-            <p><strong>Price: ${props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button buttonType='Danger' clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button buttonType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
-        </>
-    )
+    render() {
+        const ingedientSummary = Object.entries(this.props.ingredients)
+            .map((entry) => {
+                const [name, amount] = entry
+                return (
+                    <li key={name}>
+                        <span style={{textTransform: 'capitalize'}}>{name}</span>: {amount}
+                    </li>
+                )
+            })
+    
+        return(
+            <>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients</p>
+                <ul>
+                    {ingedientSummary}
+                </ul>
+                <p><strong>Price: ${this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button buttonType='Danger' clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button buttonType='Success' clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </>
+        )
+
+    }
 }
 
-export default orderSummary
+export default OrderSummary
