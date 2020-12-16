2884acf0df9e184e478304c3feb1d9715a3a254b
Michael Newman <coloradohokie@gmail.com>
2020-12-16 15:12:06 -0700

    show order details

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -26,8 +26,12 @@ class Orders extends Component {
     render() {
         return(
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order 
+                        key={order.id} 
+                        price={order.price} 
+                        ingredients={order.ingredients} />
+                ))}
             </div>
         )
     }
