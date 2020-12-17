df1176675e75a474c3fb0ef400cc35e72d94ef65
Michael Newman <coloradohokie@gmail.com>
2020-12-16 22:47:32 -0700

    display invalid fields to user

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -2,20 +2,24 @@ import classes from './Input.module.scss'
 import React from 'react'
 
 const input = props => {
-
     let inputElement= null
+    const inputClasses = [classes.InputElement]
+
+    if (props.invalid && props.shouldValidate && props.touched) {
+        inputClasses.push(classes.Invalid)
+    }
 
     switch (props.elementType) {
         case ('input'):
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed} />
+            inputElement = <input className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} onChange={props.changed} />
             break
         case ('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed} />
+            inputElement = <textarea className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} onChange={props.changed} />
             break
         case ('select'):
             inputElement = (
                 <select
-                    className={classes.InputElement}
+                    className={inputClasses.join(' ')}
                     value={props.value} onChange={props.changed}>
                     {props.elementConfig.options.map((option, index) => (
                         <option key={index} value={option.value}>{option.displayValue}</option>
@@ -23,7 +27,7 @@ const input = props => {
                 </select>)
             break
         default:
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed} />
+            inputElement = <input className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} onChange={props.changed} />
     }
     return(
         <div className={classes.Input}>
